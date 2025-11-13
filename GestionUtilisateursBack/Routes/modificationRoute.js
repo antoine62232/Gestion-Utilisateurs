b56f3e63aab21dfa30e7630928849eb0a0f35996
antoine62232 <antoinebayart@laposte.net>
2025-11-13 17:28:00 +0100

    Projet finalisé

--- GestionUtilisateursBack/Routes/modificationRoute.js
+++ GestionUtilisateursBack/Routes/modificationRoute.js
@@ -1,7 +1,8 @@
 import * as modificationController from '../Controllers/modificationController.js';
 import express from 'express';
+import { authenticateToken } from '../authMiddleware.js';
 const router = express.Router();
 
 // Route pour modifier un utilisateur par son ID
-router.put('/users/:id', modificationController.updateUser);
+router.put('/users/:id', authenticateToken, modificationController.updateUser);
 export default router;
--- GestionUtilisateursBack/Routes/suppressionRoute.js
+++ GestionUtilisateursBack/Routes/suppressionRoute.js
@@ -1,6 +1,7 @@
 import * as suppressionController from '../Controllers/suppressionController.js';
 import express from 'express';
+import { authenticateToken } from '../authMiddleware.js';
 const router = express.Router();
 // Route pour supprimer un utilisateur par son ID
-router.delete('/users/:id', suppressionController.deleteUser);
+router.delete('/users/:id', authenticateToken, suppressionController.deleteUser);
 export default router;
